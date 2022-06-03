6c0ba101e79847360d82804b82b39761cd387eb2
TiagoABarbosa <tiagosoundcar@gmail.com>
2022-06-03 18:09:43 -0300

    Enviando Projeto

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
 
 export const config = {
-    firebaseConfig: {
-        apiKey: "AIzaSyCL8r4Ffs1J_xxWB1A1D8LhiKSx3S6LQ8s",
-        authDomain: "gestao-financeira-cd760.firebaseapp.com",
-        projectId: "gestao-financeira-cd760",
-        storageBucket: "gestao-financeira-cd760.appspot.com",
-        messagingSenderId: "72902450606",
-        appId: "1:72902450606:web:f5b13839ef4c99ee4bfc3f",
-        measurementId: "G-GFDGPD5K6C"
-      }
+  firebaseConfig: {
+    apiKey: "AIzaSyBMJMknvuPS1L8lCycixfql8WvwQc_kyFo",
+    authDomain: "gestao-financeira-2022.firebaseapp.com",
+    projectId: "gestao-financeira-2022",
+    storageBucket: "gestao-financeira-2022.appspot.com",
+    messagingSenderId: "690277194215",
+    appId: "1:690277194215:web:2a1a56a348666d0badc542",
+    measurementId: "G-5Z06QTZ9K8"
+  }
 };
