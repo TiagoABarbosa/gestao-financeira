8c3000e36a7b559724e9d9114b2aba0691595bd4
TiagoABarbosa <tiagosoundcar@gmail.com>
2022-06-03 17:37:22 -0300

    Enviando Projeto

--- src/pages/Login/styles.ts
+++ src/pages/Login/styles.ts
@@ -18,9 +18,5 @@ export const HeaderText = styled.h1`
 `;
 
 export const Button = styled.button`
-    margin: 0;
-    padding: 0;
-    color: #FFF;
-    padding-top: 30px;
-    text-shadow: black 0.2em 0.1em 0.1em;
+
 `;
