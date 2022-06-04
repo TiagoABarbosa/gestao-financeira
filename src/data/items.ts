0a4e2a0316ffdb56e5dc7fa9d160f24598611734
TiagoABarbosa <tiagosoundcar@gmail.com>
2022-06-03 22:02:53 -0300

    Enviando Projeto

--- src/data/items.ts
+++ src/data/items.ts
@@ -1,10 +1,10 @@
 import { Item } from '../types/Item';
 
 export const items: Item[] = [
-    { date: new Date(2022, 4, 28), category: 'food', title: 'McDonalds', value: 69.12 },
-    { date: new Date(2022, 4, 28), category: 'fuel', title: 'Gasolina', value: 120.00 },
-    { date: new Date(2022, 4, 30), category: 'rent', title: 'Aluguel', value: 1200.00 },
-    { date: new Date(2022, 4, 31), category: 'salary', title: 'Salário Mês', value: 6000.00 },
-    { date: new Date(2022, 5, 15), category: 'salary', title: 'Adiantamento', value: 2400.00 },
+    { date: new Date(2022, 5, 28), category: 'food', title: 'McDonalds', value: 69.12 },
+    { date: new Date(2022, 5, 28), category: 'fuel', title: 'Gasolina', value: 120.00 },
+    { date: new Date(2022, 5, 30), category: 'rent', title: 'Aluguel', value: 1200.00 },
+    { date: new Date(2022, 5, 30), category: 'salary', title: 'Salário Mês', value: 6000.00 },
+    { date: new Date(2022, 6, 15), category: 'salary', title: 'Adiantamento', value: 2400.00 },
 
 ];
